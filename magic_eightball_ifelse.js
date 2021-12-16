408acd361e3d7e0a6fadd787c56fd395fc0ce6cb
Josh <joshgarcia7410@gmail.com>
2021-12-15 17:13:55 -0800

    adds ifelse file

---
+++ magic_eightball_ifelse.js
@@ -0,0 +1,17 @@
+let userName = 'Claudia'
+
+
+userName ? console.log(`Hello ${userName}!`) : console.log('Hello Stranger')
+
+let question = 'whats my fortune?'
+
+
+randomNumber = Math.floor(Math.random() * 8)
+
+if(randomNumber === 1 ) {
+ eightBall = 'Outlook bleak :('
+ 
+}
+	else if (randomNumber === 2) {
+	eightBall = 'My sources say yes'
+	}
