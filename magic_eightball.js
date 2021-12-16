b3883f44c6554f63443d20f54db60502d22ac295
Josh <joshgarcia7410@gmail.com>
2021-12-15 17:05:39 -0800

    adds switch values

--- magic_eightball.js
+++ magic_eightball.js
@@ -1,7 +1,42 @@
-let userName = ''
+let userName = 'Tom'
 
 userName ? console.log(`Hello ${userName}!`) : console.log('Hello Stranger') 
 
 let question = 'what is my fortune?'
 
-console.log(`Hello ${userName}, I see you asked ${question}`
+console.log(`Hello ${userName}, I see you asked "${question}"`)
+
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+switch (randomNumber) {
+	case 1:
+	eightBall = 'Outlook is bleak :('
+	break;
+	case 2:
+	eightBall = 'My sources say yes'
+	break;
+	case 3:
+	eightBall = "Don't count on it"
+	break;
+	case 4:
+	eightBall = 'Signs point to yes'
+	break;
+	case 5:
+	eightBall = 'Prediction hazey'
+	break;
+	case 6:
+	eightBall = 'Go with your gut'
+	break;
+	case 7:
+	eightBall = 'hmmm'
+	break;
+	case 8:
+	eightBall = 'Come back later'
+	break;
+	default:
+	eightBall = 'Try again'
+	break;
+	}
+	
+	
