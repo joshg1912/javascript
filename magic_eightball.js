7ccaece334c555c8f54080eb3f0498d568084788
Josh <joshgarcia7410@gmail.com>
2021-12-15 16:56:58 -0800

    adds fortune question

--- magic_eightball.js
+++ magic_eightball.js
@@ -1,3 +1,7 @@
 let userName = ''
 
 userName ? console.log(`Hello ${userName}!`) : console.log('Hello Stranger') 
+
+let question = 'what is my fortune?'
+
+console.log(`Hello ${userName}, I see you asked ${question}`
