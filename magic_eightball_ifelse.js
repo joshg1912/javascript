f794e99f083eb3e853b9d895429382a26ef84d3d
Josh <joshgarcia7410@gmail.com>
2021-12-15 17:22:43 -0800

    adds eightball conditionals

--- magic_eightball_ifelse.js
+++ magic_eightball_ifelse.js
@@ -1,4 +1,4 @@
-let userName = 'Claudia'
+let userName = ''
 
 
 userName ? console.log(`Hello ${userName}!`) : console.log('Hello Stranger')
@@ -6,7 +6,7 @@ userName ? console.log(`Hello ${userName}!`) : console.log('Hello Stranger')
 let question = 'whats my fortune?'
 
 
-randomNumber = Math.floor(Math.random() * 8)
+let randomNumber = Math.floor(Math.random() * 8)
 
 if(randomNumber === 1 ) {
  eightBall = 'Outlook bleak :('
@@ -15,3 +15,26 @@ if(randomNumber === 1 ) {
 	else if (randomNumber === 2) {
 	eightBall = 'My sources say yes'
 	}
+	else if (randomNumber === 3) {
+	eightBall = 'Prediction hazey'
+	}
+	else if (randomNumber === 4) {
+	eightBall = "Don't count on it"
+	}
+	else if (randomNumber === 5) {
+	eightBall = 'Turn back'
+	}
+	else if (randomNumber === 6) {
+	eightBall = 'Looking good'
+	}
+	else if (randomNumber === 7) {
+	eightBall = 'Not adviseable'
+	}
+	else if (randomNumber === 8) {
+	eightBall = 'come back later'
+	}
+	else {
+	eightBall = 'try again'
+	}
+
+userName ? console.log(`Hello ${userName}! I see you asked "${question}". Magic eightball says...${eightBall}`) : console.log(`Hello Stranger! I see you asked ${question}. Magic eightball says... ${eightBall}`)
