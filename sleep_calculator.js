befc3cc651140a89c8e81013fff565ffd45f8ae9
Josh <joshgarcia7410@gmail.com>
2021-12-17 14:28:18 -0800

    adds sleep hours and actual sleep hours func

--- sleep_calculator.js
+++ sleep_calculator.js
@@ -1,2 +1,36 @@
-get sleepHours(day){
+function getSleepHours(day){
+day = day.toLowerCase();
+     switch(day){
+	case 'monday':
+	return 7
+	break;
+	case 'tuesday':
+	return 7
+	break;
+	case 'wednesday':
+	return 8
+	break;
+	case 'thursday':
+	return 7
+	break;
+	case 'friday':
+	return 8
+	break;
+	case 'saturday':
+	return 7
+	break;
+	case 'sunday':
+	return 7
+	break;
+	default:
+	return 'enter valid day'
+	}
+};
+
+//console.log(getSleepHours('FRIDAY'));
+
+function getActualSleepHours(){
+return getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday')
 }
+
+console.log(getActualSleepHours());
