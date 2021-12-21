8d2a2293de4e66c24389d018ce6cc298010da80d
joshg1912 <joshgarcia7410@gmail.com>
2021-12-20 17:08:00 -0800

    fixes input from array to string

--- whale_talk.js
+++ whale_talk.js
@@ -1,3 +1,16 @@
-let input = ['avocado']
+let input = 'avocado'
 
-let vowels = ['a', 'e', 'i', 'o', 'u']
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+let resultArray = [];
+
+for(let i = 0; i < input.length; i++){
+  for(let o = 0; o < vowels.length; o++){
+   if(input[i] === vowels[o]){
+   resultArray.push(input[i]);
+   }
+
+  }
+}
+
+console.log(resultArray)
