244ef23abdea45f9e1b486e0aa3888274b555968
joshg1912 <joshgarcia7410@gmail.com>
2022-01-06 12:40:03 -0800

    changes dimensions to canvas

--- Sketches/sketch-01.js
+++ Sketches/sketch-01.js
@@ -1,13 +1,44 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080,1080]
 };
-
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'blue';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    
+    
+    
+const w = width * 0.10;
+const h = height * 0.10;
+const ix = width * 0.17;
+const iy = width * 0.17;
+
+const off = width * 0.02;
+let x,y;
+
+for(i = 0; i < 5; i ++){
+	for(j = 0; j < 5; j ++){
+	x = ix * i
+	y = iy * j
+	context.beginPath();
+	
+	//rectangle syntax: rect(X, Y, Height, Width)
+	context.rect(x, y, h, w);
+	context.stroke();
+	
+	
+	
+	if(Math.random() < 0.4){
+	context.lineWidth = width * 0.01
+	context.beginPath();
+	context.rect(x + off / 2, y + off / 2, h - off, w - off)
+	context.fillRect(x + 8, y + 8, h - 16, w - 16)
+	context.stroke();
+	}
+	}
+}
   };
 };
 
